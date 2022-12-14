91cfad7f3ae0a4205f58ce7c7ec8d9ed2e570995
Shafa' Tiara Tsabita Himawan <81928286+glxy-sn@users.noreply.github.com>
2022-12-14 17:12:19 +0700

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "SHAFATIARA" // TODO: replace this
+const studentId = "FE2965791" // TODO: replace this
+
+export { url, studentName, studentId }
